179f86ae453d36a3d102ffb4a3495c04095e64ec
chenyian (Yian ) <chenyian@uw.edu>
2011-08-11 21:21:39 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/factoranalytics@4 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/factorAnalytics/R/pCornishFisher.R
+++ pkg/factorAnalytics/R/pCornishFisher.R
@@ -1,8 +1,8 @@
 pCornishFisher <-
-function(q,n, skew, ekurt) {
-zp = pnorm(q)
-CDF = zp  +   1/sqrt(n) *(skew/6 * (1-q^2))*dnorm(q) + 
-     1/n *( ekurt/24*(3*q-q^3)+ (skew/6)^2/72*(10*q^3 - 15*q -q^5))*dnorm(q)
+function(q,n,skew, ekurt) {
+zq = q 
+CDF = pnorm(zq)  +   1/sqrt(n) *(skew/6 * (1-zq^2))*dnorm(zq) + 
+     1/n *( (ekurt)/24*(3*zq-zq^3)+ (skew)^2/72*(10*zq^3 - 15*zq -zq^5))*dnorm(zq)
 return(CDF)
 }
 
